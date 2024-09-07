3316cadda45a52536f4b16aa11b01044f6aac34f
LuisQuiliche <luisfernandoquiliche1@hotmail.com>
2024-09-07 09:15:00 -0500

    07092024_LuisQuiliche: Se configura estructura del proyecto y se enrutan los componentes a utilizar

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { PromocionesComponent } from './pages/promociones/promociones.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ContactanosComponent } from './pages/contactanos/contactanos.component';
+import { TermsAndCondComponent } from './pages/terms-and-cond/terms-and-cond.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "inicio", component:InicioComponent},
+    {path: "promociones", component: PromocionesComponent},
+    {path: "login", component: LoginComponent},
+    {path: "contactanos", component: ContactanosComponent},
+    {path: "t&c", component:TermsAndCondComponent}
+];
