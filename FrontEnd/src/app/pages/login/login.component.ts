c803b873bc4fd18c75712022e8cde8a2593f947c
luis2002-ctrl <58269431+luis2002-ctrl@users.noreply.github.com>
2024-09-08 21:23:56 -0500

    Update login.component.ts

--- FrontEnd/src/app/pages/login/login.component.ts
+++ FrontEnd/src/app/pages/login/login.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+
+import { CommonModule, JsonPipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Validators, ReactiveFormsModule, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(
+    private fb: FormBuilder
+    ) {}
 
-}
+    Loginform =this.fb.group({
+      persona: this.fb.group({
+        usuario: ['', Validators.required],
+        contraseña: ['', Validators.required],
+      }
+      )
+    }
+    )
+    __onsubmit() {
+      if(this.Loginform.valid) {
+        console.log(this.Loginform.value)
+      }
+      else {
+        alert("Formulario no valido")
+      }
+     }
+     
+    }
+    
